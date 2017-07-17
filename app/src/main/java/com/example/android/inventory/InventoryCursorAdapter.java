66ea1d7445e4ec32098afaf228afe280fedf73f9
Ivan <ivan.yudhi@gmail.com>
2017-07-17 13:33:05 -0700

    Rev 7

--- app/src/main/java/com/example/android/inventory/InventoryCursorAdapter.java
+++ app/src/main/java/com/example/android/inventory/InventoryCursorAdapter.java
@@ -1,86 +1,107 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.inventory;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.android.inventory.data.InventoryContract;
 import com.example.android.inventory.data.InventoryContract.InvEntry;
 
-/**
- * {@link InventoryCursorAdapter} is an adapter for a list or grid view
- * that uses a {@link Cursor} of pet data as its data source. This adapter knows
- * how to create list items for each row of pet data in the {@link Cursor}.
- */
+
 public class InventoryCursorAdapter extends CursorAdapter {
 
-    /**
-     * Constructs a new {@link InventoryCursorAdapter}.
-     *
-     * @param context The context
-     * @param c       The cursor from which to get the data.
-     */
+    /** EditText field to enter the inventory quantity */
+    //private EditText mQuantityText;
+
+    private Uri mCurrentInvUri;
+
+
     public InventoryCursorAdapter(Context context, Cursor c) {
         super(context, c, 0 /* flags */);
     }
 
-    /**
-     * Makes a new blank list item view. No data is set (or bound) to the views yet.
-     *
-     * @param context app context
-     * @param cursor  The cursor from which to get the data. The cursor is already
-     *                moved to the correct position.
-     * @param parent  The parent to which the new view is attached to
-     * @return the newly created list item view.
-     */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         // Inflate a list item view using the layout specified in list_item.xml
         return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
-    /**
-     * This method binds the inv data (in the current row pointed to by cursor) to the given
-     * list item layout. For example, the name for the current pet can be set on the name TextView
-     * in the list item layout.
-     *
-     * @param view    Existing view, returned earlier by newView() method
-     * @param context app context
-     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
-     *                correct row.
-     */
+
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
+
+        //final int productId = cursor.getInt(cursor.getColumnIndex(ProductContract.ProductEntry._ID));
+
         // Find individual views that we want to modify in the list item layout
         TextView nameTextView = (TextView) view.findViewById(R.id.name);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.quantity);
+        TextView priceTextView = (TextView) view.findViewById(R.id.price);
+        final Button saleButton = (Button) view.findViewById(R.id.sale);
 
         // Find the columns of inventory attributes that we're interested in
-        int nameColumnIndex = cursor.getColumnIndex(InvEntry.COLUMN_INV_NAME);;
+        int nameColumnIndex = cursor.getColumnIndex(InvEntry.COLUMN_INV_NAME);
+        int quantityColumnIndex = cursor.getColumnIndex(InvEntry.COLUMN_INV_QTY);
+        int priceColumnIndex = cursor.getColumnIndex(InvEntry.COLUMN_INV_PRICE);
+        int idColumnIndex = cursor.getColumnIndex(InvEntry._ID);
 
-        // Read the inventory attributes from the Cursor for the current pet
+        // Read the inventory attributes from the Cursor for the current inventory
         String invName = cursor.getString(nameColumnIndex);
+        final String invQty = cursor.getString(quantityColumnIndex);
+        String invPrice = cursor.getString(priceColumnIndex);
+        final int invId = cursor.getInt(idColumnIndex);
 
         // Update the TextViews with the attributes for the current inventory
         nameTextView.setText(invName);
+        quantityTextView.setText(invQty);
+        priceTextView.setText(invPrice);
+
+        saleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //do stuff to decrease quantity by 1
+                saleButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Uri currentProductUri = ContentUris.withAppendedId(InvEntry.CONTENT_URI, invId);
+
+                        //grab current quantity and decrease by 1
+                        String quantityString = invQty.trim();
+                        int quantity = Integer.parseInt(quantityString);
+
+                        //prevent quantity to go to negative numbers
+                        if (quantity > 0) {
+                            quantity = quantity - 1;
+                        } else quantity = 0;
+
+                        // Create a ContentValues object where column names are the keys,
+                        ContentValues values = new ContentValues();
+                        values.put(InventoryContract.InvEntry.COLUMN_INV_QTY, quantity);
+                        context.getContentResolver().update(currentProductUri, values, null, null);
+
+                        //Get current URI ???
+                        //Uri currentUri = ContentUris.withAppendedId(InvEntry.CONTENT_URI, values);
+
+                        // update a new row for an item into the provider using the ContentResolver. in the future.
+                        //getContentResolver().update(InvEntry.CONTENT_URI, values, null, null);
+                        //context.getContentResolver().update(mCurrentInvUri, values, null, null);
+
+
+                    }
+                });
+            }
+        });
     }
+
+
 }
